32b91116594083581b78104ff75e4d787e0184fe
Jonathan Stewart <xanketh@gmail.com>
2018-03-30 17:41:29 -0500

    initial commit
    it do

---
+++ uclub/src/uclub/Item.java
@@ -0,0 +1,60 @@
+package uclub;
+
+import java.text.NumberFormat;
+
+public class Item
+{
+    private int id;
+    private int quantity;
+    
+    private double price;
+    
+    private String name;
+    
+    public Item (int id, int quantity, double price, String name)
+    {
+        this.id = id;
+        this.quantity = quantity;
+        
+        this.price = price;
+        
+        this.name = name;
+    }
+    
+    public void display()
+    {        
+        NumberFormat dollars = NumberFormat.getCurrencyInstance();
+        System.out.printf("%-5s%-6s%-9s%-8s\n", id, quantity, dollars.format(price), name);
+    }
+    
+    // getters
+    public int getId(){return id;}
+
+    public int getQuantity(){return quantity;}
+
+    public double getPrice(){return price;}
+
+    public String getName(){return name;}
+
+    // setters
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public void setQuantity(int quantity)
+    {
+        this.quantity = quantity;
+    }
+
+    public void setPrice(double price)
+    {
+        this.price = price;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+}
